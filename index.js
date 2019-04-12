010651c9b14095a5febc23280036850b1f468dd5
Rafael Azuero Hurtado <razueroh@gmail.com>
2019-04-12 00:30:03 -0500

    Add automatic authentication plain

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const tls = require('tls');
 const readline = require('readline');
 const program = require('commander');
 const chalk = require('chalk');
+const Writable = require('stream').Writable;
 
 program
   .version('0.0.1', '-v, --version')
   .name('imapc')
   .description('imapc is a utility for testing the operation of an imap server')
-  .option('-p, --port <port>', 'port to connect to', 993)
   .option('-n, --hostname [hostname]', 'hostname to connect to')
+  .option('-p, --port <port>', 'port to connect to', 993)
+  .option('-u, --user [user], userid to use for authentication')
   .parse(process.argv);
 
 const {
   args,
   hostname: host,
-  port
+  port,
+  user,
 } = program;
 
 if (!host) {
@@ -47,12 +50,26 @@ if (host) {
 
   socket.setEncoding('utf8');
 
+  let authMechanism = null;
+
+  const mutableStdout = new Writable({
+    write: function (chunk, encoding, callback) {
+      if (!this.muted) {
+        process.stdout.write(chunk, encoding);
+      }
+      callback();
+    }
+  });
+
+  mutableStdout.muted = false;
+
   socket.on('data', (data) => {
-    const regex = new RegExp(/^(\*|[A-Za-z0-9]*) (OK|BAD|NO) (.*?)$/gm);
-
     let prompt = false;
+    const responseRegex = new RegExp(/^(\*|[A-Za-z0-9]*) (OK|BAD|NO) (.*?)$/gm);
+    const mechanismRegex = new RegExp(/^.*?\[CAPABILITY.*?AUTH=PLAIN\].*?$/g);
+    const authFailedRegex = new RegExp(/^.*?\[AUTHENTICATIONFAILED\].*?$/g);
 
-    const response = data.replace(regex, (full, $1, $2, $3) => {
+    const response = data.replace(responseRegex, (full, $1, $2, $3) => {
       let color;
 
       if ($2 === 'OK') {
@@ -69,6 +86,18 @@ if (host) {
       // validate if is the end of a command to show the prompt
       prompt = $1 && $1 === commandSeq && $2 && $3 && $3 !== 'LOGOUT completed.';
 
+      if (!authMechanism) {
+        initialPrompt = true;
+
+        if (mechanismRegex.test($3)) {
+          authMechanism = 'plain';
+        }
+      } else {
+        initialPrompt = authFailedRegex.test($3);
+      }
+
+      mutableStdout.muted = false;
+
       return chalk`${seq} {bold {${color} ${$2}}} ${$3}`
     });
 
@@ -76,13 +105,22 @@ if (host) {
 
     if (prompt || initialPrompt) {
       if (initialPrompt) {
-        initialPrompt = false;
+        if (user && authMechanism === 'plain') {
+          // show password prompt
+          rl.setPrompt(chalk`{bold password:} `);
+          rl.prompt();
+          mutableStdout.muted = true;
+        }
+
+        // initialPrompt = false;
       } else {
         console.timeEnd('Time elapsed');
+
+        // show imapc prompt
+        rl.setPrompt(chalk`{bold {cyan imapc>}} `);
         process.stdout.write('\n');
       }
 
-      // show prompt
       rl.prompt();
     }
   });
@@ -104,18 +142,28 @@ if (host) {
 
   const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout,
-    prompt: chalk`{bold {cyan imapc>}} `
+    output: mutableStdout,
+    prompt: chalk`{bold {cyan imapc>}} `,
+    terminal: true,
   });
 
-  rl.on('line', (line) => {
-    const regex = new RegExp(/(^[A-Za-z0-9]*).*?/);
+  const comandSeqRegex = new RegExp(/(^[A-Za-z0-9]*).*?/);
 
-    matches = regex.exec(line);
+  rl.on('line', (line) => {
+    let input = line;
+
+    if (mutableStdout.muted) {
+      const identity = Buffer.from(`\0${user}\0${line}`).toString('base64');
+      input = `A01 AUTHENTICATE PLAIN ${identity}`
+      process.stdout.write('\n');
+      rl.history = rl.history.slice(1);
+    }
+
+    matches = comandSeqRegex.exec(input);
     commandSeq = matches[1];
 
-    socket.write(line + '\r\n');
-    
+    socket.write(input + '\r\n');
+
     console.time('Time elapsed');
 
     process.stdout.write(`\n`);
